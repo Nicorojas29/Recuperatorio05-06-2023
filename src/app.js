e714f43027aaf3d5418644c455a4d8cf2cf93206
Nicorojas29 <nicolassrojas.29@gmail.com>
2023-06-05 19:24:47 -0300

    Agregue los datos json de alumnos, pedidos y viandas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require ('express');
+//const alumnosRouter = require('./routes/alumno.route')
+//const viandasRouter = require('./routes/vianda.route')
 const app = express()   
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json()) 
-
+//app.use('/api/alumnos', alumnosRouter)
+//app.use('/api/viandas', viandasRouter)
 
 app.listen(PORT, () => {console.log(`App lista escuchada en el puerto ${PORT} `) }) 
