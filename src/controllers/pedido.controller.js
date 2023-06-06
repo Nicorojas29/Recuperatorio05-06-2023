855cb66e1d654a1f80cd5814114747085573870a
Nicorojas29 <nicolassrojas.29@gmail.com>
2023-06-06 01:57:19 -0300

    Hice el las validaciones del get y post de pedidos

--- src/controllers/pedido.controller.js
+++ src/controllers/pedido.controller.js
@@ -1,5 +1,8 @@
 const pedidos = require('../../data/pedidos.json')
 
+const viandas = []; // Array de viandas disponibles
+let idCounter = 1; // Contador para generar el ID de los pedidos
+
 const getPedidos = (req, res)=> {
     res.json(pedidos).status(200) 
 }  
@@ -13,36 +16,35 @@ const getPedidosByid =(req, res) => {
       res.status(404).json({ Mensaje: 'El Pedido no fue encontrado' });
     }
   };
-
-const postPedidos = (req, res) => {
-    const { dni, tipo } = req.body;
   
+const postPedidos = (req, res) => {
+const { dni, tipo } = req.body;
     // Verificar si el alumno está habilitado
-    const alumno = { dni: 123456789, nombre: 'Nombre del Alumno', celiaco: false, edad: 18 }; // Obtener el alumno desde una fuente de datos
+const alumno = { dni: 123456789, nombre: 'Nombre del Alumno', celiaco: false, edad: 18 }; 
     if (!alumno) {
       return res.status(400).json({ error: 'Alumno no encontrado' });
     }
     if (!alumno.habilitado) {
       return res.status(400).json({ error: 'El alumno no está habilitado para realizar un pedido' });
     }
-  
-    // Verificar el tipo de vianda solicitado
-    const tiposPermitidos = ['TARTA', 'POLLO', 'PASTA', 'PIZZA', 'EMPANADAS'];
+    
+    // Verifica el tipo de vianda solicitado
+const tiposPermitidos = ['TARTA', 'POLLO', 'PASTA', 'PIZZA', 'EMPANADAS'];
     if (!tiposPermitidos.includes(tipo)) {
       return res
         .status(400)
         .json({ error: 'Tipo de vianda incorrecto. Los tipos permitidos son: TARTA, POLLO, PASTA, PIZZA, EMPANADAS' });
     }
-  
-    // Buscar una vianda que cumpla con las condiciones
-    const vianda = viandas.find((vianda) => vianda.tipo === tipo && vianda.aptoCeliaco === alumno.celiaco && vianda.stock > 0);
+
+// Buscar una vianda que cumpla con las condiciones
+const vianda = viandas.find((vianda) => vianda.tipo === tipo && vianda.aptoCeliaco === alumno.celiaco && vianda.stock > 0);
     if (!vianda) {
       return res.status(400).json({ error: 'No hay viandas disponibles que cumplan con las condiciones' });
     }
   
     // Crear el pedido
-    const fecha = new Date().toISOString().slice(0, 10);
-    const pedido = {
+const fecha = new Date().toISOString().slice(0, 10);
+const pedido = {
       id: idCounter,
       fecha,
       alumno: {
@@ -59,8 +61,10 @@ const postPedidos = (req, res) => {
       }
     };
   
-    // Actualizar la vianda y desabilitar el estado del alumno
+    // Desminuye el stock de la vianda en 1 unidad
     vianda.stock--;
+
+    // Deshabilita al alumno
     alumno.habilitado = false;
   
     // Agregar el pedido al array de pedidos
@@ -70,5 +74,5 @@ const postPedidos = (req, res) => {
     // Devolver el pedido creado
     res.json(pedido);
   };
-
+  
 module.exports = {getPedidos, getPedidosByid, postPedidos}
